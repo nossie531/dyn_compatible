39a0341535d4f182c29cfafc49317c5d07697c82
okd <nossie531@gmail.com>
2025-07-12 19:34:49 +0900

    Polish: warning settings.

--- macros/src/lib.rs
+++ macros/src/lib.rs
@@ -3,12 +3,15 @@
 //! *The author of this crate is not good at English.*
 //! *Forgive me if the document is hard to read.*
 
+#![warn(missing_docs)]
+
 use dyn_compatible_impl::parse_dyn_compatible;
 use proc_macro as pm;
 use proc_macro2::TokenStream;
 
 mod dyn_compatible_impl;
 
+#[allow(missing_docs)]
 #[proc_macro_attribute]
 pub fn dyn_compatible(attr: pm::TokenStream, item: pm::TokenStream) -> pm::TokenStream {
     let attr = TokenStream::from(attr);
